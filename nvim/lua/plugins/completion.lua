5dc5b806c44d9401ef6ec10968f0f3a41b47bc7b
Wetar <azrilharis75@gmail.com>
2025-10-09 16:43:50 +0700

    Changed the completions plugin theme on neovim

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -64,7 +64,7 @@ return {
 				menu = {
 					border = "single",
 					scrollbar = false,
-					winblend = 1,
+					winblend = 0,
 					draw = {
 						treesitter = { "lsp" },
 					},
@@ -73,7 +73,7 @@ return {
 					window = {
 						border = "single",
 						scrollbar = false,
-						winblend = 1,
+						winblend = 0,
 					},
 					auto_show = true,
 				},
@@ -134,7 +134,7 @@ return {
 				window = {
 					border = "single",
 					scrollbar = false,
-					winblend = 1,
+					winblend = 0,
 				},
 			},
 			fuzzy = { implementation = "prefer_rust_with_warning" },
